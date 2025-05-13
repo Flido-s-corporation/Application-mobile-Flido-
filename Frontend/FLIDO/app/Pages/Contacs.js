0a80f010bd382d53ca02b9a7a1ef447ad4bca389
ALEXIS37ADIGBLI <154370828+ALEXIS37ADIGBLI@users.noreply.github.com>
2025-05-13 06:23:41 +0000

    Modification de l'interface en fonction des couleurs

--- Frontend/FLIDO/app/Pages/Contacs.js
+++ Frontend/FLIDO/app/Pages/Contacs.js
@@ -15,49 +15,56 @@ const Contacs = () => {
       id: "1",
       name: "Emily (Angleterre)",
       message: "Ce fait longtemps ! Tu vas bien ?",
+      source: require("../../assets/images/pp/1.png"),
     },
     {
       id: "2",
       name: "Emily (Angleterre)",
       message: "Ce fait longtemps ! Tu vas bien ?",
+      source: require("../../assets/images/pp/2.png"),
     },
     {
       id: "3",
       name: "Anna (Allemagne)",
       message: "Dis, on se voit bientôt pour discuter un peu ?",
+      source: require("../../assets/images/pp/3.png"),
     },
-    { id: "4", name: "Camila (Brésil)", message: "Quoi de neuf chez toi ?" },
+    { id: "4", name: "Camila (Brésil)", message: "Quoi de neuf chez toi ?", source: require("../../assets/images/pp/4.png"), },
+
     {
       id: "5",
       name: "Yuki (Japon)",
       message: "Yuki, ça me fait plaisir de te parler",
+      source: require("../../assets/images/pp/5.png"),
     },
     {
       id: "6",
       name: "Michael (États-Unis)",
       message: "Toujours aussi sérieuse au travail ?",
+      source: require("../../assets/images/pp/6.png"),
     },
     {
       id: "7",
       name: "Minho (Corée du Sud)",
       message: "Tas toujours des plans cool",
+      source: require("../../assets/images/pp/7.png"),
     },
-    { id: "8", name: "Diego (Espagne)", message: "On se capte bientôt" },
-    { id: "9", name: "Fatima (Maroc)", message: "Broi Ça fait un bail" },
+    { id: "8", name: "Diego (Espagne)", message: "On se capte bientôt", source: require("../../assets/images/pp/8.png"), },
+    { id: "9", name: "Fatima (Maroc)", message: "Broi Ça fait un bail", source: require("../../assets/images/pp/9.png"), },
   ];
 
   return (
     <View className="flex-1">
       {/* Header */}
-      <View className="bg-[#817D7D] pt-[5]">
+      <View className="bg-[#DECFBC] pt-[5]">
         <View className="flex-row justify-between items-center p-[3]">
           <Image
             className="h-[100] w-[100] rounded-full"
             source={require("../../assets/images/lg.jpg")}
           />
           <Image
-            className="h-[80] w-[80] rounded-[20]"
-            source={require("../../assets/images/lg.jpg")}
+            className="h-[80] w-[90] rounded-[20]"
+            source={require("../../assets/s.png")}
           />
         </View>
 
@@ -81,23 +88,23 @@ const Contacs = () => {
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={{ paddingVertical: 10 }}
           data={[
-            { id: "1", source: require("../../assets/images/lg.jpg") },
-            { id: "2", source: require("../../assets/images/lg.jpg") },
-            { id: "3", source: require("../../assets/images/lg.jpg") },
-            { id: "4", source: require("../../assets/images/lg.jpg") },
-            { id: "5", source: require("../../assets/images/lg.jpg") },
-            { id: "6", source: require("../../assets/images/lg.jpg") },
-            { id: "7", source: require("../../assets/images/lg.jpg") },
+            { id: "1", source: require("../../assets/images/1.png") },
+            { id: "2", source: require("../../assets/images/2.png") },
+            { id: "3", source: require("../../assets/images/3.png") },
+            { id: "4", source: require("../../assets/images/4.png") },
+            { id: "5", source: require("../../assets/images/5.png") },
+            { id: "6", source: require("../../assets/images/1.png") },
+            { id: "7", source: require("../../assets/images/3.png") },
             // ... autres items
           ]}
           renderItem={({ item }) => (
             <Image
-              className="h-[80] w-[80] rounded-full mx-[1] "
+              className="h-[80] w-[80] rounded-full mx-[1] justify-between mr-[10] "
               source={item.source}
             />
           )}
           keyExtractor={(item) => item.id}
-          className="bg-[#2F2F2F2F]"
+          className="bg-[#85664A]"
         />
       </View>
 
@@ -110,11 +117,11 @@ const Contacs = () => {
             <View className="flex-row items-center p-4">
               <Image
                 className="h-[60] w-[60] rounded-full mr-4"
-                source={require("../../assets/images/lg.jpg")}
+                source={item.source}
               />
               <View className="flex-1">
-                <Text className="text-lg font-semibold">{item.name}</Text>
-                <Text className="text-gray-600">{item.message}</Text>
+                <Text className="text-white font-semibold">{item.name}</Text>
+                <Text className=" text-white">{item.message}</Text>
               </View>
             </View>
             {/* Barre de séparation */}
@@ -122,7 +129,7 @@ const Contacs = () => {
           </View>
         )}
         keyExtractor={(item) => item.id}
-        className="flex-1 bg-[#2F2F2F2F]"
+        className="flex-1 bg-[#85664A]"
       />
 
       <View>
@@ -135,7 +142,7 @@ const Contacs = () => {
       </View>
 
       <View className="bg-slate-600 py-[40] items-center" />
-      <TouchableOpacity className="bg-white rounded-full h-[50] w-[100] items-center justify-center absolute bottom-[20] right-[20]">
+      <TouchableOpacity className="bg-white rounded-full h-[50] w-[100] items-center justify-center absolute bottom-[20] right-[170]">
         <Text className="font-[500]">Fr </Text>
       </TouchableOpacity>
     </View>
