485386d0fb2370beff7736943473b41d370fb4ff
ALEXIS37ADIGBLI <154370828+ALEXIS37ADIGBLI@users.noreply.github.com>
2025-05-18 16:59:16 +0000

    ok

--- Frontend/FLIDO/app/index.js
+++ Frontend/FLIDO/app/index.js
@@ -4,7 +4,7 @@ import "../global.css"
 
 const index = () => {
     
-    return <Redirect href={"/Pages/disc"} /> //Permet de rediriger automatiquement l'application
+    return <Redirect href={"/Pages/accueil"} /> //Permet de rediriger automatiquement l'application
     //vers la page d'accueil une fois l'application démarrée avec expo-router
 }
 
